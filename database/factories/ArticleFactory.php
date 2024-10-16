67b3b4d515020c01e4adc585cab9d502ded8947d
AnyssaO <anyssa.ouahib@outlook.com>
2024-10-16 16:03:09 +0200

    added "generate fake articles"

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
@@ -17,7 +20,18 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-        ];
+            'title' => fake()->bs(),
+            'body' => fake()->realTextBetween($minNbChars = 500, $maxNbChars = 2000),
+            'img_path' => function() {
+                $randomName = Str::uuid();
+                $imageUrl = "https://picsum.photos/1024/768.webp?random={$randomName}";
+                $path = "images/{$randomName}.webp";
+                Storage::disk('public')->put($path, file_get_contents($imageUrl));        
+                
+                return $path;
+            },
+            'published_at' =>fake()->dateTimeBetween('-2 months', '+1 month'),
+            'user_id' => User::get()->random()->id,
+        ];  
     }
 }
